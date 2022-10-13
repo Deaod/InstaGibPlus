Deaod/InstaGibPlus

commit ad1ba6bf01a88b26369907d46a75cd6f455d3ae8

Fixed bug where switching weapon when mid-switch would take longer than the specified down time settings.

Classes/ST_ImpactHammer.uc
@@ -239,7 +239,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * 0.4 );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().HammerDownAnimSpeed() );
 	else
 		PlayAnim('Down', GetWeaponSettings().HammerDownAnimSpeed(), 0.05);
 }

Classes/ST_ShockRifle.uc
@@ -146,7 +146,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * 0.4 );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().ShockDownAnimSpeed() );
 	else
 		PlayAnim('Down', GetWeaponSettings().ShockDownAnimSpeed(), 0.05);
 }

Classes/ST_SniperRifle.uc
@@ -149,7 +149,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * 0.4 );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().SniperDownAnimSpeed() );
 	else
 		PlayAnim('Down', GetWeaponSettings().SniperDownAnimSpeed(), 0.05);
 

Classes/ST_SuperShockRifle.uc
@@ -106,7 +106,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * 0.4 );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().ShockDownAnimSpeed() );
 	else
 		PlayAnim('Down', GetWeaponSettings().ShockDownAnimSpeed(), 0.05);
 }

Classes/ST_Translocator.uc
@@ -97,7 +97,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * 0.36 );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().TranslocatorDownAnimSpeed() );
 	else if ( bTTargetOut )
 		PlayAnim('Down2', GetWeaponSettings().TranslocatorDownAnimSpeed(), 0.05);
 	else

Classes/ST_UT_Eightball.uc
@@ -234,7 +234,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * 0.4 );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().EightballDownAnimSpeed() );
 	else
 		PlayAnim('Down', GetWeaponSettings().EightballDownAnimSpeed(), 0.05);
 }

Classes/ST_UT_FlakCannon.uc
@@ -156,7 +156,7 @@ function SetSwitchPriority(pawn Other)
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * 0.4 );
+		TweenAnim( AnimSequence, AnimFrame * etWeaponSettings().FlakDownAnimSpeed() );
 	else if ( AmmoType.AmmoAmount < 1 )
 		TweenAnim('Select', GetWeaponSettings().FlakDownTime + 0.05);
 	else

Classes/ST_WarheadLauncher.uc
@@ -78,7 +78,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * 0.4 );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().WarheadDownAnimSpeed() );
 	else
 		PlayAnim('Down', GetWeaponSettings().WarheadDownAnimSpeed(), 0.05);
 }

commit 445a7ab2e4fd6c57075811b817bf60ddae71a797

Fixed issue with FWS introduced in PR #17.

Classes/ST_ImpactHammer.uc
@@ -239,7 +239,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().HammerDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().HammerDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().HammerDownAnimSpeed(), 0.05);
 }

Classes/ST_ShockRifle.uc
@@ -146,7 +146,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().ShockDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().ShockDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().ShockDownAnimSpeed(), 0.05);
 }

Classes/ST_SniperRifle.uc
@@ -149,7 +149,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().SniperDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().SniperDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().SniperDownAnimSpeed(), 0.05);
 

Classes/ST_SuperShockRifle.uc
@@ -106,7 +106,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().ShockDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().ShockDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().ShockDownAnimSpeed(), 0.05);
 }

Classes/ST_Translocator.uc
@@ -97,7 +97,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().TranslocatorDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().TranslocatorDownTime );
 	else if ( bTTargetOut )
 		PlayAnim('Down2', GetWeaponSettings().TranslocatorDownAnimSpeed(), 0.05);
 	else

Classes/ST_UT_Eightball.uc
@@ -234,7 +234,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().EightballDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().EightballDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().EightballDownAnimSpeed(), 0.05);
 }

Classes/ST_UT_FlakCannon.uc
@@ -156,7 +156,7 @@ function SetSwitchPriority(pawn Other)
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().FlakDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().FlakDownTime );
 	else if ( AmmoType.AmmoAmount < 1 )
 		TweenAnim('Select', GetWeaponSettings().FlakDownTime + 0.05);
 	else

Classes/ST_WarheadLauncher.uc
@@ -78,7 +78,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().WarheadDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().WarheadDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().WarheadDownAnimSpeed(), 0.05);
 }

Classes/ST_enforcer.uc
@@ -137,7 +137,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().EnforcerDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().EnforcerDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().EnforcerDownAnimSpeed(), 0.05);
 }

Classes/ST_minigun2.uc
@@ -346,7 +346,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().MinigunDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().MinigunDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().MinigunDownAnimSpeed(), 0.05);
 }

Classes/ST_ripper.uc
@@ -78,7 +78,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().RipperDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().RipperDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().RipperDownAnimSpeed(), 0.05);
 }

Classes/ST_ut_biorifle.uc
@@ -78,7 +78,7 @@ simulated function PlaySelect() {
 
 simulated function TweenDown() {
 	if ( IsAnimating() && (AnimSequence != '') && (GetAnimGroup(AnimSequence) == 'Select') )
-		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().BioDownAnimSpeed() );
+		TweenAnim( AnimSequence, AnimFrame * GetWeaponSettings().BioDownTime );
 	else
 		PlayAnim('Down', GetWeaponSettings().BioDownAnimSpeed(), 0.05);
 }